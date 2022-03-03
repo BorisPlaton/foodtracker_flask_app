5bedae124e28ea9ffb34fb26c57b9f8da9c5fabf
Kirill Panchencko <videokirills@gmail.com>
2022-03-03 23:49:43 +0200

    adding date

--- index.py
+++ index.py
@@ -1,17 +1,65 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, g
+import datetime, sqlite3
 
 app = Flask(__name__)
 
 
+def connect_db():  # Подключение базы данных
+    sql = sqlite3.connect("food.db")
+    sql.row_factory = sqlite3.Row
+    return sql
+
+
+def get_db():  # Получение базы данных
+    if not hasattr(g, "sqlite3"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):  # Закрытие соединения с базой данных при получении страницы
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        pass
-    return render_template("index.html")
+    """Главная страница"""
+    db = get_db()
+
+    if request.method == "POST":    # Если пользователь добавляет новый день
+        date_log = request.form["date"]
+
+        try:  # Проверяем ввод даты, в ином случае возвращаем на главную страницу
+            date = datetime.datetime.strptime(date_log, "%Y-%m-%d")
+            date_sql = datetime.datetime.strftime(date, "%Y%m%d")
+            db.execute("""
+                INSERT INTO log_date (entry_date)
+                VALUES
+                    (?);
+            """, [date_sql])
+            db.commit()
+        except ValueError:
+            return redirect(url_for("index"))
+
+    cur = db.execute("""
+        SELECT entry_date FROM log_date;
+    """)
+    results = cur.fetchall()  # Получаем даты для преобразования в другой вид
+    user_date = []
+
+    for i in results:
+        info = {}
+        pd = datetime.datetime.strptime(str(i['entry_date']), "%Y%m%d")
+        info["entry_date"] = datetime.datetime.strftime(pd, "%B %d, %Y")
+        user_date.append(info)
+
+    return render_template("index.html", results=user_date)
 
 
 @app.route("/add_food", methods=["POST", "GET"])
 def add_food():
+    """Страница добавление нового типа еды"""
     if request.method == "POST":
         pass
     return render_template("add_food.html")
@@ -19,6 +67,7 @@ def add_food():
 
 @app.route("/day/<date>", methods=["POST", "GET"])
 def day(date):
+    """Показывает список еды за день"""
     if request.method == "POST":
         pass
     return render_template("add_food.html")
